7e58f77661ce7753da297f8b1ca63b01729f0194
Joey Lee <joeylee@comp.nus.edu>
2024-10-08 12:04:32 +0800

    fix: message logic

--- message.py
+++ message.py
@@ -3,24 +3,26 @@ import time
 
 class Message:
 
-    def convert_to_bits(self, value):
-        return list(bin(value)[2:].zfill(10))
+    def convert_to_bits(self, value, num_of_bits):
+        return list(bin(value)[2:].zfill(num_of_bits))
 
     def send(self, key, value):
-        if (key == "x"):
-            print(f"Sending x coordinate: {value}")
+        if (key == "x" or key == "y"):
+            if key == "x":
+                print(f"Sending x coordinate: {value}")
+            else:
+                print(f"Sending y coordinate: {value}")
             # self.send_x(self.convert_to_bits(value))
-            pass
-        elif (key == "y"):
-            print(f"Sending y coordinate: {value}")
-            pass
-        pass
+            pins = [13, 15, 19, 21, 23, 27, 29, 31, 33, 35]
+            value_in_bits = self.convert_to_bits(value, 10)
+        elif (key == "header"):
+            print(f"Sending header: {value}")
+            pins = [4, 17]
+            value_in_bits = self.convert_to_bits(value, 10)
 
-    def send_xy(self, value):
-        GPIO.setmode(GPIO.BOARD)
-        pins = [13, 15, 19, 21, 23, 27, 29, 31, 33, 35]
-        for i in range(len(value)):
-            bit = value[i]
+
+        for i in range(len(value_in_bits)):
+            bit = value_in_bits[i]
             xy_pin = pins[i]
             if bit == "1":
                 # GPIO.output(xy_pin, GPIO.HIGH)
@@ -28,8 +30,6 @@ class Message:
                 GPIO.output(xy_pin, GPIO.HIGH)
             else:
                 GPIO.setup(xy_pin, GPIO.IN)
+                # GPIO.output(xy_pin, GPIO.LOW) # For new relay board
 
-        GPIO.setup(xy_pin, GPIO.OUT)
-        time.sleep(0.1)
-        GPIO.output(xy_pin, GPIO.LOW)
         pass
