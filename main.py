e281728202ad9ae2061010146020304b112d85cb
Joey Lee <joeylee@comp.nus.edu>
2024-10-15 13:45:44 +0800

    feat: documentation

--- main.py
+++ main.py
@@ -6,39 +6,65 @@ send = Send()
 toolRecognition = ToolRecognition()
 receive = Receive()
 
+
+
 # Inspect the tool (cam 1)
+def main():
+    """
+        Steps:
+        3. rpi receives status 1
+        4. rpi inspects and observes
+        5. rpi sends xy coords to arm
 
-#Send the x and y coordinates to the robot arm
-while True:
-    status = receive.receive_from_arm()
+        10. rpi receives status 2
+        11. rpi inspects
+        12. rpi sends the type to the arm
+        
+        16. arduino reads load cell
+        17. rpi receives load cell “tool is placed” from arduino
+        18. rpi tells arm tool is placed
+    """ 
+
+    # Setup
     receive.connect_to_mqtt()
-    if status == 1: # Observation point
-        # TODO: Inspect
 
-        # Observe
-        x_coord, y_coord, class_id = toolRecognition.locate()[0]
-        send.send_x(int(x_coord))
-        time.sleep(3)
-        send.send_y(int(y_coord))
-        time.sleep(3)
-        print(class_id)
+    #Send the x and y coordinates to the robot arm
+    while True:
+        # Wait for status 2
+        status = receive.receive_from_arm()
 
-        
-    elif status == 2: # Inspection point 
-        # TODO: Inspect
-        pass
+        if status == 1: # Observation point
+            # TODO: Inspect
 
-        # Send the type of the tool (which box to put)
-        send.send_type(class_id)
-        time.sleep(3)
+            # Observe
+            x_coord, y_coord, class_id = toolRecognition.locate()[0]
 
-        # Arm: goes to the respective box
-
-        # Check if the tool is placed (load cell, take from arduino)
-        # If placed, go back to inspection point
-        while not receive.isPlaced == "placed":
-            time.sleep(1)
-        
-        if receive.isPlaced == "placed":
-            send.send_load_cell(1)
+            # Send data to the robot arm
+            send.send_x(int(x_coord))
             time.sleep(3)
+            send.send_y(int(y_coord))
+            time.sleep(3)
+            print(class_id)
+
+            
+        elif status == 2: # Inspection point 
+            # TODO: Inspect
+            pass
+
+            # Send the type of the tool (which box to put)
+            send.send_type(class_id)
+            time.sleep(3)
+
+            # Arm: goes to the respective box
+
+            # Check if the tool is placed (load cell, take from arduino)
+            # If placed, go back to inspection point
+            while not receive.isPlaced == "placed":
+                time.sleep(1)
+            
+            if receive.isPlaced == "placed":
+                send.send_load_cell(1)
+                time.sleep(3)
+
+if __name__ == "__main__":
+    main()
--- receive.py
+++ receive.py
@@ -6,6 +6,7 @@ class Receive:
     def __init__(self):
         self.status = 0
         self.isPlaced = "not placed"
+        self.loadcell_topic = "loadcell"
 
     def convert_from_bits(self, bits_list):
         # Convert the list of bits to a string
@@ -17,11 +18,12 @@ class Receive:
         GPIO.setup((3, 5), GPIO.IN)
         self.status = self.convert_from_bits([int(GPIO.input(3)), int(GPIO.input(15))]) 
         return self.status
+    
     def connect_to_mqtt(self):
         client = mqtt.Client()
         client.connect("172.20.10.3", 1883, 60)
-        client.subscribe("loadcell")
-        client.message_callback_add("loadcell", self.loadcell_callback)
+        client.subscribe(self.loadcell_topic)
+        client.message_callback_add(self.load_cell_topic, self.loadcell_callback)
         client.loop_forever()
 
 
